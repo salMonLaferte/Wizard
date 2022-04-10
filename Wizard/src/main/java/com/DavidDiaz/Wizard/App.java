2146a8993ba23009d10d821d2f9d821d4cc2265a
salMonLaferte <david_diaz@ciencias.unam.mx>
2022-04-10 13:14:06 -0500

    Card Draw demo complete

--- Wizard/src/main/java/com/DavidDiaz/Wizard/App.java
+++ Wizard/src/main/java/com/DavidDiaz/Wizard/App.java
@@ -1,11 +1,16 @@
 package com.DavidDiaz.Wizard;
 
+import java.util.IllegalFormatFlagsException;
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -24,20 +29,36 @@ public class App extends Application {
         mainWindow.setTitle("Wizard by David Diaz");
         mainWindow.setScene(scene);
         mainWindow.getIcons().add(icon);
-        drawCard(new RegularCard(3,14), root);
+        for(int i=0; i< 13; i++){
+            drawCard(new RegularCard(1, i), root,  i*100, 0);
+        }
         mainWindow.show();
     }
 
-    public void drawCard(Card card, Group root){
+    
+    public void drawCard(Card card, Group root, int x, int y){
+        Group imgGroup = new Group();
         Image img = new Image(card.getImage());
         ImageView imgView = new ImageView(img);
-        root.getChildren().add(imgView);
-        if(card instanceof DumbCard){
+        imgGroup.getChildren().add(imgView);
+        if(card instanceof DumbCard || card instanceof WizardCard){
             return;
         }
-        if(card instanceof WizardCard){
-            return;
-        }
+        Rectangle r = new Rectangle(40, 40, Color.BLACK);
+        imgGroup.getChildren().add(r);
+        r.setX(img.getWidth() -40);
+        r.setY(img.getHeight()-40);
+        RegularCard c = (RegularCard)card;
+        String number = "" + c.number;
+        Text text = new Text( number );
+        text.setFill(Color.WHITE);
+        imgGroup.getChildren().add(text);
+        text.setX(img.getWidth() -40);
+        text.setY(img.getHeight()-5 );
+        text.setFont(Font.font(40));
+        imgGroup.setTranslateX(x);
+        imgGroup.setTranslateY(y);
+        root.getChildren().add(imgGroup);
     }
     
 }
